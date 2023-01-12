ec63b1a69a7dd0c029402aef3c6a24565ab1d87e
Matthew Hobbs-Hurrell <matthewhobbshurrell@icloud.com>
2023-01-12 21:50:31 +0000

    Add summernote fields to recipe model in admin area

--- recipeapp/admin.py
+++ recipeapp/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import Recipe
+from django_summernote.admin import SummernoteModelAdmin
 
-# Register your models here.
+
+@admin.register(Recipe)
+class RecipeAdmin(SummernoteModelAdmin):
+
+    summernote_fields = (
+        'method',
+        'ingredients',
+    )
