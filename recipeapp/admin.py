86d2c9849d267654e5f5d7c1d96b906fc184522a
Matthew Hobbs-Hurrell <matthewhobbshurrell@icloud.com>
2023-01-13 21:35:17 +0000

    Add recipe and comment post filters and functions to admin.py

--- recipeapp/admin.py
+++ recipeapp/admin.py
@@ -1,12 +1,35 @@
 from django.contrib import admin
-from .models import Recipe
+from .models import Recipe, Comment
 from django_summernote.admin import SummernoteModelAdmin
 
 
 @admin.register(Recipe)
 class RecipeAdmin(SummernoteModelAdmin):
 
+    prepopulated_fields = {'slug': ('title',)}
+    search_fields = ['title', 'method']
+    list_filter = ('status', 'created_on', 'featured')
+    list_display = ('id', 'title', 'slug', 'author', 'created_on', 'featured')
     summernote_fields = (
         'method',
         'ingredients',
     )
+    actions = ['add_featured', 'remove_featured']
+
+    def add_featured(self, request, queryset):
+        queryset.update(featured=True)
+
+    def remove_featured(self, request, queryset):
+        queryset.update(featured=False)
+
+
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+
+    search_fields = ['recipe', 'name', 'body']
+    list_filter = ('recipe', 'name', 'created_on', 'approved')
+    list_display = ('name', 'email', 'recipe', 'created_on', 'approved')
+    actions = ['approve_comments']
+
+    def approve_comments(self, request, queryset):
+        queryset.update(approved=True)
