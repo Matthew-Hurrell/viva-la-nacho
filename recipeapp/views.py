2feaea20124a8d2f6ea1b03fbf2c86bb0863e02b
Matthew Hobbs-Hurrell <matthewhobbshurrell@icloud.com>
2023-01-17 21:05:39 +0000

    Add recipelist view to views.py

--- recipeapp/views.py
+++ recipeapp/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django.views import generic
+from .models import Recipe
 
-# Create your views here.
+
+class RecipeList(generic.ListView):
+    model = Recipe
+    queryset = Recipe.objects.filter(status=1).order_by('-created_on')
+    template_name = 'index.html'
+    paginate_by = 6
