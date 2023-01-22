2f8dbacf07c7f2d5b129861ece2b698c53fad177
Matthew Hobbs-Hurrell <matthewhobbshurrell@icloud.com>
2023-01-22 00:38:08 +0000

    Change views recipe list class to add featured recipe

--- recipeapp/views.py
+++ recipeapp/views.py
@@ -3,8 +3,22 @@ from django.views import generic
 from .models import Recipe
 
 
+# class RecipeList(generic.ListView):
+#     model = Recipe
+#     queryset = Recipe.objects.filter(status=1).order_by('-created_on')
+#     featured_recipe = Recipe.objects.get(featured=True)
+#     template_name = 'index.html'
+#     paginate_by = 6
+#     context_object_name = 'latest_recipe_list'
+
+
 class RecipeList(generic.ListView):
-    model = Recipe
-    queryset = Recipe.objects.filter(status=1).order_by('-created_on')
-    template_name = 'index.html'
-    paginate_by = 6
+    context_object_name = "data"
+    template_name = "index.html"
+
+    def get_queryset(self):
+        myset = {
+            "latest_recipes": Recipe.objects.filter(status=1).order_by('-created_on'),
+            "featured_recipe": Recipe.objects.get(featured=True),
+        }
+        return myset
