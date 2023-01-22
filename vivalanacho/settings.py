e4cb019095791e4eeb90f1c04d05950dca9cabcd
Matthew Hobbs-Hurrell <matthewhobbshurrell@icloud.com>
2023-01-22 11:54:52 +0000

    add debug = False in settings.py

--- vivalanacho/settings.py
+++ vivalanacho/settings.py
@@ -26,7 +26,9 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = 'DEVELOPMENT' in os.environ
+DEBUG = False
+
+# 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = ['viva-la-nacho.herokuapp.com', 'localhost']
 
