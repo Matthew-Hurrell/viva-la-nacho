d422e82a08ea5a8e8e84443007cbfb589e9772b2
Matthew Hobbs-Hurrell <matthewhobbshurrell@icloud.com>
2023-01-22 13:15:39 +0000

    Add debug environ if statement in settings.py

--- vivalanacho/settings.py
+++ vivalanacho/settings.py
@@ -26,9 +26,7 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
-
-# 'DEVELOPMENT' in os.environ
+DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = ['viva-la-nacho.herokuapp.com', 'localhost']
 
