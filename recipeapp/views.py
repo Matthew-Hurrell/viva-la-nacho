5f0f816da729ce1b6c6911d4c3248bba1014cf44
Matthew Hobbs-Hurrell <matthewhobbshurrell@icloud.com>
2023-01-22 20:28:46 +0000

    Add recipe details view

--- recipeapp/views.py
+++ recipeapp/views.py
@@ -1,5 +1,5 @@
-from django.shortcuts import render
-from django.views import generic
+from django.shortcuts import render, get_object_or_404
+from django.views import generic, View
 from .models import Recipe
 from django.db.models import Count
 
@@ -14,6 +14,7 @@ from django.db.models import Count
 
 
 class RecipeList(generic.ListView):
+    model = Recipe
     context_object_name = "data"
     template_name = "index.html"
 
@@ -24,3 +25,24 @@ class RecipeList(generic.ListView):
             "most_popular_recipes": Recipe.objects.annotate(like_count=Count('likes')).order_by('-like_count'),
         }
         return myset
+
+
+class RecipeDetails(View):
+
+    def get(self, request, slug, *args, **kwargs):
+        queryset = Recipe.objects.filter(status=1)
+        recipe = get_object_or_404(queryset, slug=slug)
+        comments = recipe.comments.filter(approved=True).order_by('created_on')
+        liked = False
+        if recipe.likes.filter(id=self.request.user.id).exists():
+            liked = True
+
+        return render(
+            request,
+            "recipe_details.html",
+            {
+                "recipe": recipe,
+                "comments": comments,
+                "liked": liked,
+            },
+        )
