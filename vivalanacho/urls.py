75391d02812bc0865971d20c5deade372083ab1f
Matthew Hobbs-Hurrell <matthewhobbshurrell@icloud.com>
2023-01-24 21:46:48 +0000

    Add allauth accounts/ to urls.py

--- vivalanacho/urls.py
+++ vivalanacho/urls.py
@@ -20,4 +20,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('summernote/', include('django_summernote.urls')),
     path('', include('recipeapp.urls'), name='recipe_urls'),
+    path('accounts/', include('allauth.urls')),
 ]
