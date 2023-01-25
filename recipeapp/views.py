c8124097f3e5c680e3ec9150e57620432768dd0e
Matthew Hobbs-Hurrell <matthewhobbshurrell@icloud.com>
2023-01-25 22:48:17 +0000

    Add post class to recipe details view

--- recipeapp/views.py
+++ recipeapp/views.py
@@ -2,15 +2,7 @@ from django.shortcuts import render, get_object_or_404
 from django.views import generic, View
 from .models import Recipe
 from django.db.models import Count
-
-
-# class RecipeList(generic.ListView):
-#     model = Recipe
-#     queryset = Recipe.objects.filter(status=1).order_by('-created_on')
-#     featured_recipe = Recipe.objects.get(featured=True)
-#     template_name = 'index.html'
-#     paginate_by = 6
-#     context_object_name = 'latest_recipe_list'
+from .forms import CommentForm
 
 
 class RecipeList(generic.ListView):
@@ -43,6 +35,39 @@ class RecipeDetails(View):
             {
                 "recipe": recipe,
                 "comments": comments,
+                "commented": False,
                 "liked": liked,
+                "comment_form": CommentForm()
+            },
+        )
+
+    def post(self, request, slug, *args, **kwargs):
+        queryset = Recipe.objects.filter(status=1)
+        recipe = get_object_or_404(queryset, slug=slug)
+        comments = recipe.comments.filter(approved=True).order_by('created_on')
+        liked = False
+        if recipe.likes.filter(id=self.request.user.id).exists():
+            liked = True
+
+        comment_form = CommentForm(data=request.POST)
+
+        if comment_form.is_valid():
+            comment_form.instance.email = request.user.email
+            comment_form.instance.name = request.user.username
+            comment = comment_form.save(commit=False)
+            comment.recipe = recipe
+            comment.save()
+        else:
+            comment_form = CommentForm()
+
+        return render(
+            request,
+            "recipe_details.html",
+            {
+                "recipe": recipe,
+                "comments": comments,
+                "commented": True,
+                "liked": liked,
+                "comment_form": CommentForm()
             },
         )
