13f1e710a2bba0e519f23b0d528f9b698cfd3348
Matthew Hobbs-Hurrell <matthewhobbshurrell@icloud.com>
2023-01-26 21:36:44 +0000

    Add recipe like class view with post method

--- recipeapp/views.py
+++ recipeapp/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, reverse
 from django.views import generic, View
+from django.http import HttpResponseRedirect
 from .models import Recipe
 from django.db.models import Count
 from .forms import CommentForm
@@ -71,3 +72,16 @@ class RecipeDetails(View):
                 "comment_form": CommentForm()
             },
         )
+
+
+class RecipeLike(View):
+
+    def post(self, request, slug):
+        recipe = get_object_or_404(Recipe, slug=slug)
+
+        if recipe.likes.filter(id=request.user.id).exists():
+            recipe.likes.remove(request.user)
+        else:
+            recipe.likes.add(request.user)
+
+        return HttpResponseRedirect(reverse('recipe_details', args=[slug]))
