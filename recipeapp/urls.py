70c5eb02bc636a9a88f8e905895834bceedee8d8
Matthew Hobbs-Hurrell <matthewhobbshurrell@icloud.com>
2023-01-28 18:30:20 +0000

    Bug fix: Change order of URLs in urls.py

--- recipeapp/urls.py
+++ recipeapp/urls.py
@@ -4,6 +4,7 @@ from django.urls import path
 
 urlpatterns = [
     path('', views.RecipeList.as_view(), name='home'),
-    path('<slug:slug>/', views.RecipeDetails.as_view(), name='recipe_details'),
     path('like/<slug:slug>', views.RecipeLike.as_view(), name='recipe_like'),
+    path('post_recipe/', views.PostRecipe.as_view(), name='post_recipe'),
+    path('<slug:slug>/', views.RecipeDetails.as_view(), name='recipe_details'),
 ]
