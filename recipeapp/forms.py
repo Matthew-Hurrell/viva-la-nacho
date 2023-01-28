31e5f5e14985552a1ae8f9e53e7169def4c6b4b3
Matthew Hobbs-Hurrell <matthewhobbshurrell@icloud.com>
2023-01-28 18:29:06 +0000

    Add recipe form with fields to forms.py

--- recipeapp/forms.py
+++ recipeapp/forms.py
@@ -1,4 +1,4 @@
-from .models import Comment
+from .models import Comment, Recipe
 from django import forms
 
 
@@ -6,3 +6,25 @@ class CommentForm(forms.ModelForm):
     class Meta:
         model = Comment
         fields = ('body',)
+
+
+class RecipeForm(forms.ModelForm):
+    class Meta:
+        model = Recipe
+        fields = (
+            'title',
+            'category',
+            'featured_image',
+            'gallery_image_1',
+            'gallery_image_2',
+            'gallery_image_3',
+            'excerpt',
+            'prep_time',
+            'cooking_time',
+            'difficulty',
+            'serves',
+            'allergens',
+            'ingredients',
+            'method',
+            'status',
+        )
