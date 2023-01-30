f747664e0d02b54c283dd36779fe82c2a286a083
Matthew Hobbs-Hurrell <matthewhobbshurrell@icloud.com>
2023-01-30 22:07:20 +0000

    Add edit recipe path to urls.py

--- recipeapp/urls.py
+++ recipeapp/urls.py
@@ -7,5 +7,6 @@ urlpatterns = [
     path('like/<slug:slug>', views.RecipeLike.as_view(), name='recipe_like'),
     path('post_recipe/', views.PostRecipe.as_view(), name='post_recipe'),
     path('my_recipes/', views.MyRecipes.as_view(), name='my_recipes'),
+    path('recipe/edit/<int:pk>/', views.EditRecipe.as_view(), name='edit_recipe'),
     path('<slug:slug>/', views.RecipeDetails.as_view(), name='recipe_details'),
 ]
