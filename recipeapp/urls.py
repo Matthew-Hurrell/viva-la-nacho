c6018bad3d8cfb5ccfd737383831019f8dda055d
Matthew Hobbs-Hurrell <matthewhobbshurrell@icloud.com>
2023-01-31 21:44:27 +0000

    Add delete recipe url path

--- recipeapp/urls.py
+++ recipeapp/urls.py
@@ -8,5 +8,6 @@ urlpatterns = [
     path('post_recipe/', views.PostRecipe.as_view(), name='post_recipe'),
     path('my_recipes/', views.MyRecipes.as_view(), name='my_recipes'),
     path('recipe/edit/<int:pk>/', views.EditRecipe.as_view(), name='edit_recipe'),
+    path('recipe/delete/<int:pk>/', views.DeleteRecipe, name='delete_recipe'),
     path('<slug:slug>/', views.RecipeDetails.as_view(), name='recipe_details'),
 ]
