18dd49714106900641cdae20be913aa399e3f650
Matthew Hobbs-Hurrell <matthewhobbshurrell@icloud.com>
2023-02-01 17:46:43 +0000

    Add recipe unlike javascript functionality

--- static/js/script.js
+++ static/js/script.js
@@ -1,4 +1,4 @@
-// Toggle Nav Icons
+// Toggle Nav Icons Functionality 
 const nav_button = document.querySelector('#main-nav-mobile-button');
 const burger_icon = nav_button.querySelector('.burger-menu');
 const exit_icon = nav_button.querySelector('.exit-menu');
@@ -10,7 +10,7 @@ nav_button.addEventListener('click', () => {
     mobile_menu.classList.toggle('hidden');
 })
 
-// Cancel Buttons
+// Cancel Button Functionality
 if ( document.getElementsByClassName('cancel').length > 0 ) {
     const cancel_buttons = document.getElementsByClassName('cancel');
     const form = document.querySelector('.form-content');
@@ -24,7 +24,7 @@ if ( document.getElementsByClassName('cancel').length > 0 ) {
     }
 }
 
-// Recipe Delete Button
+// Recipe Delete Functionality
 if ( document.getElementsByClassName('my-recipe-card-content').length > 0 ) {
     const my_recipe_cards = document.getElementsByClassName('my-recipe-card-content');
     
@@ -42,6 +42,28 @@ if ( document.getElementsByClassName('my-recipe-card-content').length > 0 ) {
         cancel_button.addEventListener('click', () => {
             recipe_text_content.classList.toggle('hidden');
             delete_recipe_notification.classList.toggle('hidden');
+        })
+    }
+}
+
+// Recipe Unlike Functionality
+if ( document.getElementsByClassName('my-favourites-card-content').length > 0 ) {
+    const my_favourites_cards = document.getElementsByClassName('my-favourites-card-content');
+    
+    for (let recipe of my_favourites_cards) {
+        const recipe_text_content = recipe.querySelector('.recipe-text-content');
+        const unlike_recipe_notification = recipe.querySelector('.unlike-recipe-notification');
+        const unlike_button = recipe.querySelector('.unlike-button');
+        const cancel_button = unlike_recipe_notification.querySelector('.cancel-unlike');
+
+        unlike_button.addEventListener('click', () => {
+            recipe_text_content.classList.toggle('hidden');
+            unlike_recipe_notification.classList.toggle('hidden');
+        })
+
+        cancel_button.addEventListener('click', () => {
+            recipe_text_content.classList.toggle('hidden');
+            unlike_recipe_notification.classList.toggle('hidden');
         })
     }
 }
