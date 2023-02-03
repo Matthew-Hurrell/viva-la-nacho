fdd80112569b799abee98407281b0193d97a1ab9
Matthew Hobbs-Hurrell <matthewhobbshurrell@icloud.com>
2023-02-03 14:17:13 +0000

    Add comments to admin.py

--- recipeapp/admin.py
+++ recipeapp/admin.py
@@ -5,31 +5,61 @@ from django_summernote.admin import SummernoteModelAdmin
 
 @admin.register(Recipe)
 class RecipeAdmin(SummernoteModelAdmin):
+    # Recipe Admin
 
+    # Prepopulated Slug Field
     prepopulated_fields = {'slug': ('title',)}
+
+    # Search Fields
     search_fields = ['title', 'method']
+
+    # List Filter
     list_filter = ('status', 'created_on', 'featured')
-    list_display = ('title', 'slug', 'author', 'created_on', 'status', 'featured')
+
+    # List Display Fields
+    list_display = (
+        'title',
+        'slug',
+        'author',
+        'created_on',
+        'status',
+        'featured'
+    )
+
+    # Summernote Fields
     summernote_fields = (
         'method',
         'ingredients',
     )
+
+    # Recipe Admin Area Functions
     actions = ['add_featured', 'remove_featured']
 
+    # Add Featured Recipe Function
     def add_featured(self, request, queryset):
         queryset.update(featured=True)
 
+    # Remove Featured Recipe Function
     def remove_featured(self, request, queryset):
         queryset.update(featured=False)
 
 
 @admin.register(Comment)
 class CommentAdmin(admin.ModelAdmin):
+    # Comment Admin
 
+    # Search Fields
     search_fields = ['recipe', 'name', 'body']
+
+    # List Filter
     list_filter = ('recipe', 'name', 'created_on', 'approved')
+
+    # List Display Fields
     list_display = ('name', 'email', 'recipe', 'created_on', 'approved')
+
+    # Comment Admin Area Function
     actions = ['approve_comments']
 
+    # Approve Comments Function
     def approve_comments(self, request, queryset):
         queryset.update(approved=True)
