2737ab7bcc20f494cb31ee9d6a6f2aedea5657ad
Matthew Hobbs-Hurrell <matthewhobbshurrell@icloud.com>
2023-02-05 16:24:38 +0000

    Fix formatting errors in urls.py

--- recipeapp/urls.py
+++ recipeapp/urls.py
@@ -23,13 +23,16 @@ urlpatterns = [
     path('my_favourites/', views.MyFavourites.as_view(), name='my_favourites'),
 
     # Path for Unlike Recipe (Remove from My Favourites)
-    path('recipe/unlike/<int:pk>', views.UnlikeRecipe.as_view(), name='unlike_recipe'),
+    path('recipe/unlike/<int:pk>', views.UnlikeRecipe.as_view(),\
+         name='unlike_recipe'),
 
     # Path for Edit Recipe
-    path('recipe/edit/<int:pk>/', views.EditRecipe.as_view(), name='edit_recipe'),
+    path('recipe/edit/<int:pk>/', views.EditRecipe.as_view(),\
+         name='edit_recipe'),
 
     # Path for Delete Recipe
-    path('recipe/delete/<int:pk>/', views.DeleteRecipe.as_view(), name='delete_recipe'),
+    path('recipe/delete/<int:pk>/', views.DeleteRecipe.as_view(),\
+         name='delete_recipe'),
 
     # Path for Recipe Details
     path('<slug:slug>/', views.RecipeDetails.as_view(), name='recipe_details'),
