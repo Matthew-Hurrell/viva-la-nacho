9c34093bcf9b0cfa4f872acd36914d5d56f09ebb
Matthew Hobbs-Hurrell <matthewhobbshurrell@icloud.com>
2023-02-07 21:07:27 +0000

    Remove unnecessary comments from models.py

--- recipeapp/models.py
+++ recipeapp/models.py
@@ -11,10 +11,8 @@ class ModifiedArrayField(ArrayField):
     Allows for checkbox multiple select fields to create an array.
     """
 
-    # Form Field Function
     def formfield(self, **kwargs):
 
-        # Defaults
         defaults = {
             "form_class": forms.MultipleChoiceField,
             "choices": self.base_field.choices,
@@ -93,94 +91,49 @@ class Recipe(models.Model):
     Assigns fields and methods for the recipe model.
     """
 
-    # Title Field
     title = models.CharField(max_length=200, unique=True)
-
-    # Slug Field
     slug = models.SlugField(max_length=200, unique=True)
-
-    # ID Field (Primary Key)
     id = models.AutoField(primary_key=True)
-
-    # Author Field
     author = models.ForeignKey(
         User,
         on_delete=models.CASCADE,
         related_name="recipe_posts"
     )
-
-    # Category Field
     category = ModifiedArrayField(
         models.CharField(
             choices=CATEGORIES,
             max_length=100
         )
     )
-
-    # Featured Image Field
     featured_image = CloudinaryField('image', default='placeholder')
-
-    # Gallery Image 1 Field
     gallery_image_1 = CloudinaryField('image', blank=True, null=True)
-
-    # Gallery Image 2 Field
     gallery_image_2 = CloudinaryField('image', blank=True, null=True)
-
-    # Gallery Image 3 Field
     gallery_image_3 = CloudinaryField('image', blank=True, null=True)
-
-    # Excerpt Field
     excerpt = models.TextField()
-
-    # Prep Time Field
     prep_time = models.PositiveIntegerField()
-
-    # Cooking Time Field
     cooking_time = models.PositiveIntegerField()
-
-    # Difficulty Field
     difficulty = models.IntegerField(choices=DIFFICULTIES)
-
-    # Serving Field
     serves = models.PositiveIntegerField()
-
-    # Allergens Field
     allergens = ModifiedArrayField(
         models.CharField(
             choices=ALLERGIES,
             max_length=100
         )
     )
-
-    # Ingredients Field
     ingredients = models.TextField()
-
-    # Method Field
     method = models.TextField()
-
-    # Created On Field
     created_on = models.DateTimeField(auto_now_add=True)
-
-    # Updated On Field
     updated_on = models.DateTimeField(auto_now=True)
-
-    # Status Field
     status = models.IntegerField(choices=STATUS, default=0)
-
-    # Likes Field
     likes = models.ManyToManyField(
         User,
         related_name="recipe_likes",
         blank=True
     )
-
-    # Featured Field
     featured = models.BooleanField(default=False)
 
     class Meta:
-        # Meta Class
 
-        # Ordering
         ordering = ['-created_on']
 
     # Title String Function
@@ -206,32 +159,19 @@ class Comment(models.Model):
     Assigns fields and methods for the comment model.
     """
 
-    # Recipe Field (Foreign Key)
     recipe = models.ForeignKey(
         Recipe,
         on_delete=models.CASCADE,
         related_name="comments"
     )
-
-    # Name Field
     name = models.CharField(max_length=80)
-
-    # Email Field
     email = models.EmailField()
-
-    # Body Field
     body = models.TextField()
-
-    # Created On Field
     created_on = models.DateTimeField(auto_now_add=True)
-
-    # Approved Field
     approved = models.BooleanField(default=False)
 
     class Meta:
-        # Meta Class
 
-        # Ordering
         ordering = ['created_on']
 
     # Title String Function
