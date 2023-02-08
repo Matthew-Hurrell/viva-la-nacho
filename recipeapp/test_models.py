dd2378006790b4e6bd3a2c05f2c35e59a7b2f315
Matthew Hobbs-Hurrell <matthewhobbshurrell@icloud.com>
2023-02-08 20:34:47 +0000

    Add test_models.py file

---
+++ recipeapp/test_models.py
@@ -0,0 +1,13 @@
+# from django.test import TestCase
+# from .models import Comment, Recipe
+
+
+# class TestModels(TestCase):
+
+#     def test_string_method_returns_title(self):
+#         recipe = Recipe.objects.create(title='Test recipe')
+#         self.assertEqual(str(item), 'Test recipe')
+
+#     def test_featured_defaults_to_false(self):
+#         recipe = Item.objects.create(title='Test recipe')
+#         self.assertFalse(recipe.featured)
