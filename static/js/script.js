c6deaaa86d4d4015ddc35fed4aefade2ca892eea
Matthew Hobbs-Hurrell <matthewhobbshurrell@icloud.com>
2023-02-16 22:02:18 +0000

    Change javascript form validation

--- static/js/script.js
+++ static/js/script.js
@@ -83,132 +83,222 @@ if ( document.getElementsByClassName('notification-close-button').length > 0 ) {
 }
 
 // Form Validation
-if ( ( document.getElementsByClassName('post-recipe').length > 0 ) || ( document.getElementsByClassName('edit-recipe').length > 0 ) ) {
-    let recipe_form;
+// if ( ( document.getElementsByClassName('post-recipe').length > 0 ) || ( document.getElementsByClassName('edit-recipe').length > 0 ) ) {
+//     let recipe_form;
 
     // Assign recipe form variable to whatever form is being displayed
-    if ( document.getElementsByClassName('post-recipe').length > 0 ) {
-        recipe_form = document.forms["post-recipe"];
-    } else if ( document.getElementsByClassName('edit-recipe').length > 0 ) {
-        recipe_form = document.forms["edit-recipe"];
-    }
+    // if ( document.getElementsByClassName('post-recipe').length > 0 ) {
+    //     recipe_form = document.forms["post-recipe"];
+    // } else if ( document.getElementsByClassName('edit-recipe').length > 0 ) {
+    //     recipe_form = document.forms["edit-recipe"];
+    // }
 
     // Click submit button function
-    function buttonclick(){
-        let submit_button = document.querySelector(".recipe-submit-button");
-        submit_button.click();
-    }
+    // function buttonclick(){
+    //     let submit_button = document.querySelector(".recipe-submit-button");
+    //     submit_button.click();
+    // }
 
     // Strip HTML from summernote fields function
+    // function stripHTML(html){
+    //     let doc = new DOMParser().parseFromString(html, 'text/html');
+    //     return doc.body.textContent || "";
+    // }
+
+    // Validate form function
+//     function validateRecipeForm() {
+
+//         let title = recipe_form["title"].value;
+//         let category_checkboxes = document.querySelector('#div_id_category').querySelectorAll('input[type="checkbox"]:checked');
+//         let prep_time = recipe_form["prep_time"].value;
+//         let cooking_time = recipe_form["cooking_time"].value;
+//         let serves = recipe_form["serves"].value;
+//         let difficulty = recipe_form["difficulty"].value;
+//         let excerpt = recipe_form["excerpt"].value;
+//         let allergen_checkboxes = document.querySelector('#div_id_allergens').querySelectorAll('input[type="checkbox"]:checked');
+//         let ingredients = recipe_form["ingredients"].value;
+//         let method = recipe_form["method"].value;
+//         let featured_image = recipe_form["featured_image"].value;
+//         let featured_image_has_current = document.querySelector('#div_id_featured_image').getElementsByTagName('a');
+
+//         if ( ( title.innerText == "" ) || ( title.trim().length == 0 ) ) {
+//             alert("Form error - Title must be filled out");
+//             return false;
+//         }
+//         if ( category_checkboxes.length == 0 ) {
+//             alert("Form error - Please select a category");
+//             return false;
+//         }
+//         if ( prep_time == "" ) {
+//             alert("Form error - Prep time must be filled out");
+//             return false;
+//         }
+//         if ( prep_time >= 600 ) {
+//             alert("Form error - Prep time is too large");
+//             return false;
+//         }
+//         if ( cooking_time == "" ) {
+//             alert("Form error - Cooking time must be filled out");
+//             return false;
+//         }
+//         if ( cooking_time >= 600 ) {
+//             alert("Form error - Cooking time is too large");
+//             return false;
+//         }
+//         if ( serves == "" ) {
+//             alert("Form error - Serves must be filled out");
+//             return false;
+//         }
+//         if ( serves > 10 ) {
+//             alert("Form error - Serving is too large");
+//             return false;
+//         }
+//         if ( difficulty == "" ) {
+//             alert("Form error - Difficulty must be filled out");
+//             return false;
+//         }
+//         if ( ( excerpt == "" ) || ( excerpt.trim().length == 0 ) ) {
+//             alert("Form error - Excerpt must be filled out");
+//             return false;
+//         }
+//         if ( excerpt.length >= 250 ) {
+//             alert("Form error - Excerpt is too long. Limit is 250 characters");
+//             return false;
+//         }
+//         if ( allergen_checkboxes.length == 0 ) {
+//             alert("Form error - Please select an allergen, or select 'none' for no allergens");
+//             return false;
+//         }
+//         if ( ( ingredients == "" ) || ( stripHTML(ingredients).trim().length == 0 ) ) {
+//             alert("Form error - Ingredients must be filled out");
+//             return false;
+//         }
+//         if ( ( method == "" ) || ( stripHTML(method).trim().length == 0 ) ) {
+//             alert("Form error - Method must be filled out");
+//             return false;
+//         }
+//         if ( ( featured_image_has_current.length == 0 ) && ( featured_image == "" ) ) {
+//             alert("Form error - Please upload at least one image using the first image field");
+//             return false;
+//         }
+//         if ( ! false ) {
+//             buttonclick();
+//         }
+//     }
+// }
+
+// 
+
+// Comment Form Validation
+
+// if ( document.getElementsByClassName('comment-form').length > 0 ) {
+//     let comment_form = document.forms["comment-form"];
+
+    // Click submit button function
+    // function buttonclick(){
+    //     let submit_button = document.querySelector(".comment-submit-button");
+    //     submit_button.click();
+    // }
+
+    // Validate form function
+//     function validateCommentForm() {
+
+//         let body = comment_form["body"].value;
+
+//         if ( ( body.innerText == "" ) || ( body.trim().length == 0 ) ) {
+//             alert("Form error - Comment body must be filled out");
+//             return false;
+//         }
+//         if ( ! false ) {
+//             buttonclick();
+//         }
+//     }
+// }
+
+// Post Recipe Form Validation
+
+if ( document.getElementsByClassName('post-recipe').length > 0 ) {
+    let form = document.forms["post-recipe"];
+
     function stripHTML(html){
         let doc = new DOMParser().parseFromString(html, 'text/html');
         return doc.body.textContent || "";
     }
 
-    // Validate form function
-    function validateRecipeForm() {
+    form.addEventListener('submit', event => {
 
-        let title = recipe_form["title"].value;
-        let category_checkboxes = document.querySelector('#div_id_category').querySelectorAll('input[type="checkbox"]:checked');
-        let prep_time = recipe_form["prep_time"].value;
-        let cooking_time = recipe_form["cooking_time"].value;
-        let serves = recipe_form["serves"].value;
-        let difficulty = recipe_form["difficulty"].value;
-        let excerpt = recipe_form["excerpt"].value;
+        let title = form["title"].value;
+        let category_checkboxes = document.getElementById('div_id_category').querySelectorAll('input[type="checkbox"]:checked');
+        let prep_time = form["prep_time"].value;
+        let cooking_time = form["cooking_time"].value;
+        let serves = form["serves"].value;
+        let difficulty = form["difficulty"].value;
+        let excerpt = form["excerpt"].value;
         let allergen_checkboxes = document.querySelector('#div_id_allergens').querySelectorAll('input[type="checkbox"]:checked');
-        let ingredients = recipe_form["ingredients"].value;
-        let method = recipe_form["method"].value;
-        let featured_image = recipe_form["featured_image"].value;
+        let ingredients = form["ingredients"].value;
+        let method = form["method"].value;
+        let featured_image = form["featured_image"].value;
         let featured_image_has_current = document.querySelector('#div_id_featured_image').getElementsByTagName('a');
 
-        if ( ( title.innerText == "" ) || ( title.trim().length == 0 ) ) {
-            alert("Form error - Title must be filled out");
-            return false;
+        if ( ( title == "" ) || ( title.trim().length == 0 ) ) {
+            event.preventDefault();
+            alert('Form error - Please enter a valid title.');
         }
         if ( category_checkboxes.length == 0 ) {
+            event.preventDefault();
             alert("Form error - Please select a category");
-            return false;
         }
         if ( prep_time == "" ) {
+            event.preventDefault();
             alert("Form error - Prep time must be filled out");
-            return false;
         }
         if ( prep_time >= 600 ) {
+            event.preventDefault();
             alert("Form error - Prep time is too large");
-            return false;
         }
         if ( cooking_time == "" ) {
+            event.preventDefault();
             alert("Form error - Cooking time must be filled out");
-            return false;
         }
         if ( cooking_time >= 600 ) {
+            event.preventDefault();
             alert("Form error - Cooking time is too large");
-            return false;
         }
         if ( serves == "" ) {
+            event.preventDefault();
             alert("Form error - Serves must be filled out");
-            return false;
         }
         if ( serves > 10 ) {
+            event.preventDefault();
             alert("Form error - Serving is too large");
-            return false;
         }
         if ( difficulty == "" ) {
+            event.preventDefault();
             alert("Form error - Difficulty must be filled out");
-            return false;
         }
         if ( ( excerpt == "" ) || ( excerpt.trim().length == 0 ) ) {
+            event.preventDefault();
             alert("Form error - Excerpt must be filled out");
-            return false;
         }
         if ( excerpt.length >= 250 ) {
+            event.preventDefault();
             alert("Form error - Excerpt is too long. Limit is 250 characters");
-            return false;
         }
         if ( allergen_checkboxes.length == 0 ) {
+            event.preventDefault();
             alert("Form error - Please select an allergen, or select 'none' for no allergens");
-            return false;
         }
         if ( ( ingredients == "" ) || ( stripHTML(ingredients).trim().length == 0 ) ) {
+            event.preventDefault();
             alert("Form error - Ingredients must be filled out");
-            return false;
         }
         if ( ( method == "" ) || ( stripHTML(method).trim().length == 0 ) ) {
+            event.preventDefault();
             alert("Form error - Method must be filled out");
-            return false;
         }
         if ( ( featured_image_has_current.length == 0 ) && ( featured_image == "" ) ) {
+            event.preventDefault();
             alert("Form error - Please upload at least one image using the first image field");
-            return false;
         }
-        if ( ! false ) {
-            buttonclick();
-        }
-    }
-}
-
-// Comment Form Validation
-
-if ( document.getElementsByClassName('comment-form').length > 0 ) {
-    let comment_form = document.forms["comment-form"];
-
-    // Click submit button function
-    function buttonclick(){
-        let submit_button = document.querySelector(".comment-submit-button");
-        submit_button.click();
-    }
-
-    // Validate form function
-    function validateCommentForm() {
-
-        let body = comment_form["body"].value;
-
-        if ( ( body.innerText == "" ) || ( body.trim().length == 0 ) ) {
-            alert("Form error - Comment body must be filled out");
-            return false;
-        }
-        if ( ! false ) {
-            buttonclick();
-        }
-    }
+    });
 }
